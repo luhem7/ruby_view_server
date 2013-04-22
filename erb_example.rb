76989e98af4aa09e1358a5c1111b625aca84caa5
Mehul Gangavelli <mehul.gang@gmail.com>
2013-04-22 19:12:56 -0400

    Finished Exercise 1

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,9 @@
 require 'erb'
 
 x = 42
-template = ERB.new "The value of x is: <%= x %>"
+myString = "The value of x is: <%= x %>\n" +
+	"Now we are going to increment x in-situ:\n" +
+	"The value of x is: <%= x += 1 %>\n "
+	
+template = ERB.new myString
 puts template.result(binding)
